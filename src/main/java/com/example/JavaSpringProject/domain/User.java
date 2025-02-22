1ce9989ad18277022aa1ec755aca7d500939a085
Hungnvh25 <144328260+Hungnvh25@users.noreply.github.com>
2025-02-22 23:31:33 +0700

    update model product role order

--- src/main/java/com/example/JavaSpringProject/domain/User.java
+++ src/main/java/com/example/JavaSpringProject/domain/User.java
@@ -4,8 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,7 +17,10 @@ public class User {
     private String fullName;
     private String address;
     private String phone;
+    private String avatar;
+    
 
+    
     public long getId() {
         return id;
     }
@@ -64,10 +69,20 @@ public class User {
         this.phone = phone;
     }
 
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
     @Override
     public String toString() {
         return "User [id=" + id + ", email=" + email + ", password=" + password + ", fullName=" + fullName
-                + ", address=" + address + ", phone=" + phone + "]";
+                + ", address=" + address + ", phone=" + phone + ", avatar=" + avatar + "]";
     }
+
+    
 
 }
